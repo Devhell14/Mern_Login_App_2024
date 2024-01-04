35f44da89cece304f332f26f92cbbdf9a064690d
Devhell69 <paelalit@gmail.com>
2024-01-04 23:25:16 +0700

    debug change env

--- server/database/conn.js
+++ server/database/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async() =>{
     try{
         mongoose.set('strictQuery', false);
-        await mongoose.connect(process.env.DATABASE)
+        await mongoose.connect(process.env.MONGODB_URI)
         
         console.log('Connect DB Success')
     }catch(err){
